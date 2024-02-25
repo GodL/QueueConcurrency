832264bdaf821abc32c6451f0fdb83b4302a3751
GodL <547188371@qq.com>
2024-02-25 15:02:11 +0800

    1.0.2

---
+++ Sources/QueueConcurrency/Concurrency+Group.swift
@@ -0,0 +1,50 @@
+//
+//  Concurrency+Group.swift
+//
+//
+//  Created by L God on 2024/2/25.
+//
+
+fileprivate struct OrderElement<Element: Sendable>: Comparable, Sendable {
+    static func < (lhs: OrderElement<Element>, rhs: OrderElement<Element>) -> Bool {
+        lhs.order < rhs.order
+    }
+    
+    static func == (lhs: OrderElement<Element>, rhs: OrderElement<Element>) -> Bool {
+        lhs.order == rhs.order
+    }
+    
+    let order: Int
+    
+    let element: Element
+}
+
+public func withOrderTask<T, S: Sequence>(sequence: S, _ operation: @escaping @Sendable (S.Element) async -> T ) async -> [T] where T : Sendable {
+    return await withTaskGroup(of: OrderElement<T>.self) { group in
+        for (index, element) in sequence.enumerated() {
+            group.addTask {
+                let value = await operation(element)
+                return OrderElement(order: index, element: value)
+            }
+        }
+        return await group.reduce([]) { partialResult, orderElement in
+            partialResult + [orderElement]
+        }.sorted(by: <).map(\.element)
+    }
+}
+
+public func withThrowingOrderTask<T, S: Sequence>(sequence: S, _ operation: @escaping @Sendable (S.Element) async throws -> T) async throws -> [T] where T : Sendable {
+    return try await withThrowingTaskGroup(of: OrderElement<T>.self) { group in
+        for (index, element) in sequence.enumerated() {
+            group.addTask {
+                let value = try await operation(element)
+                return OrderElement(order: index, element: value)
+            }
+        }
+        return try await group.reduce([]) { partialResult, orderElement in
+            partialResult + [orderElement]
+        }.sorted(by: <).map(\.element)
+    }
+}
+
+
